0368f9a5685838ea85525277434bfbd6a7d4459f
Hanna Franchesca Herradura <hhannafranchesca03@gmail.com>
2025-11-09 12:49:20 +0800

    Fix MySQL connection for Railway

--- run.py
+++ run.py
@@ -1,19 +1,15 @@
+from dotenv import load_dotenv
 import os
 from app import app
 
-# Build DATABASE_URL from Railway MySQL variables
-MYSQL_USER = os.environ.get("MYSQL_USER")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
-MYSQL_HOST = os.environ.get("MYSQL_HOST")
-MYSQL_PORT = os.environ.get("MYSQL_PORT")
-MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
+load_dotenv()
 
-DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
-# Set it for SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
-# Normal Flask code
+#TO ACTIVATE DEBUGG MODE
 if __name__ == "__main__":
+    # Get port from environment variable (Railway provides this automatically)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    
+    # Run Flask on all interfaces and correct port
+    app.run(host="0.0.0.0", port=port, debug=True)
