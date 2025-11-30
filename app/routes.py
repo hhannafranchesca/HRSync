2ade808a5e7b294401ecf952cfad78b3a46a1fe4
Hanna Franchesca Herradura <hhannafranchesca03@gmail.com>
2025-11-30 22:52:55 +0800

    Fix  header PDF reports

--- app/routes.py
+++ app/routes.py
@@ -6107,7 +6107,7 @@ def generate_leave_application_pdf(permit_id):
         head_approver_position=head_approver_position,
         head_approver_id=head_approver_id,
         current_stage=permit.current_stage or 'N/A',
-        credit_balance=employee.credit_balance 
+        credit_balance=credit_balance
     )
     pdf.add_instructions_page()
 
